5ecbef7ac5eb6bc6b0238238516256f6c11947d9
Anton Volovik <volovik.anton@gmail.com>
2019-12-06 00:29:38 +0300

    Bite 101. f-strings and a simple if-else

---
+++ Bite 101. f-strings and a simple if-else/driving.py
@@ -0,0 +1,15 @@
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    name = str(name)
+    name = name.strip()
+    if age >= 18:
+        print (f'{name} is allowed to drive')
+    else:
+        print (f'{name} is not allowed to drive')
+    return
+
+#allowed_driving("DRIVERNAME",18)
