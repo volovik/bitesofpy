26a4458302000043c29f7a7f4ce52cf2b05d7837
Anton Volovik <volovik.anton@gmail.com>
2019-12-08 03:25:59 +0300

    Loop through games_won's dict (key, value) pairs (dict.items)

---
+++ Bite 103. Loop through a dictionary and pluralise a word/winners.py
@@ -0,0 +1,28 @@
+games_won = dict(sara=0, bob=1, tim=5, julian=3, jim=1)
+
+
+def print_game_stats(games_won):
+    """Loop through games_won's dict (key, value) pairs (dict.items)
+       printing (print, not return) how many games each person has won,
+       pluralize 'game' based on number.
+
+       Expected output (ignore the docstring's indentation):
+
+       sara has won 0 games
+       bob has won 1 game
+       tim has won 5 games
+       julian has won 3 games
+       jim has won 1 game
+
+       (Note that as of Python 3.7 - which we're using atm - dict insert order
+       is retained so no sorting is required for this Bite.)
+    """
+    for gamers,num_games in games_won.items():
+        if num_games == 1:
+            games = "game"
+        else :
+            games = "games"
+        print(f"{gamers} has won {num_games} {games}")
+    return
+
+#print_game_stats(games_won)
